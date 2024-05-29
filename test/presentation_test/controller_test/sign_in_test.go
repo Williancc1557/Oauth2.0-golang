9f272d3e2242ad7fc2397802efce6c7d19e92622
willian <willian.cacoelho@gmail.com>
2024-05-29 00:33:41 -0300

    test(controllers): should return status code 400 if reset return an error

--- test/presentation_test/controller_test/sign_in_test.go
+++ test/presentation_test/controller_test/sign_in_test.go
@@ -182,3 +182,60 @@ func TestSignInControllerInvalidPasswordCredentials(t *testing.T) {
 		t.Fatalf("unexpected error: got %v want %v", responseBody.Error, "invalid credentials")
 	}
 }
+
+func TestSignInControllerResettingRefreshTokenError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockEncrypter := mocks.NewMockEncrypter(ctrl)
+	mockGetAccountByEmail := mocks.NewMockGetAccountByEmail(ctrl)
+	mockResetRefreshToken := mocks.NewMockResetRefreshToken(ctrl)
+
+	account := &models.AccountModel{
+		Id:           "fake-account-id",
+		Name:         "fake-account-name",
+		Email:        "fake-account-email",
+		Password:     "fake-account-password",
+		RefreshToken: "fake-account-refresh-token",
+	}
+
+	signInController := &controllers.SignInController{
+		GetAccountByEmail: mockGetAccountByEmail,
+		Encrypter:         mockEncrypter,
+		ResetRefreshToken: mockResetRefreshToken,
+	}
+
+	mockGetAccountByEmail.EXPECT().Get(email).Return(account, nil)
+	mockEncrypter.EXPECT().Compare(password, account.Password).Return(true)
+	mockResetRefreshToken.EXPECT().Reset(account.Id).Return("", errors.New("fake-error"))
+
+	requestBody, err := json.Marshal(&controllers.SignInControllerBody{
+		Email:    email,
+		Password: password,
+	})
+	if err != nil {
+		t.Fatalf("an error occurred while marshaling body: %v", err)
+	}
+
+	httpRequest := &protocols.HttpRequest{
+		Body:   io.NopCloser(bytes.NewReader(requestBody)),
+		Header: nil,
+	}
+
+	httpResponse := signInController.Handle(*httpRequest)
+
+	if httpResponse.StatusCode == http.StatusOK {
+		t.Errorf("unexpected status code: got %d want %d", httpResponse.StatusCode, http.StatusOK)
+	}
+
+	var responseBody protocols.ErrorResponse
+	err = json.NewDecoder(httpResponse.Body).Decode(&responseBody)
+
+	if err != nil {
+		t.Fatalf("an error occurred while decoding response body: %v", err)
+	}
+
+	if responseBody.Error != "an error ocurred while resetting refresh token" {
+		t.Fatalf("unexpected error: got '%v' want '%v'", responseBody.Error, "an error ocurred while resetting refresh token")
+	}
+}
