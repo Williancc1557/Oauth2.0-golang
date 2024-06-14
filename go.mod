438493e79a317a6d697ecd2e587e5e1a9a2bc80e
willian <willian.cacoelho@gmail.com>
2024-06-14 17:13:50 -0300

    refactor(main): remove godotenv library

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.22.4
 require (
 	github.com/go-playground/validator/v10 v10.22.0
 	github.com/google/uuid v1.6.0
-	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.24.0
 )
--- main.go
+++ main.go
@@ -5,21 +5,14 @@ import (
 	"net/http"
 
 	"github.com/Williancc1557/Oauth2.0-golang/internal/setup"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	port := ":8080"
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Printf("Some error occured. Err: %s", err)
-		return
-	}
-
 	fmt.Println("server is running with port", port)
 
-	err = http.ListenAndServe(port, setup.Server())
+	err := http.ListenAndServe(port, setup.Server())
 
 	if err != nil {
 		fmt.Println("error ocurred: ", err.Error())
