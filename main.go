3dfdd0a32d786244b5a9595e09010d146f5fb645
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-07-26 17:29:19 -0300

    chore: apply graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Williancc1557/Oauth2.0-golang/internal/setup"
 	"github.com/Williancc1557/Oauth2.0-golang/internal/setup/config"
@@ -10,14 +15,32 @@ import (
 
 func main() {
 	port := ":8080"
-
 	config.LoadEnvFile(".env")
 
-	fmt.Println("server is running with port", port)
+	log.Println("server is running with port", port)
 
-	err := http.ListenAndServe(port, setup.Server())
-
-	if err != nil {
-		fmt.Println("error ocurred: ", err.Error())
+	sm := http.Server{
+		Addr:         port,
+		Handler:      setup.Server(),
+		IdleTimeout:  60 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
 	}
+
+	go func() {
+		err := sm.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	sig := <-sigChan
+	log.Println("received terminate, graceful shutdown", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	sm.Shutdown(tc)
 }
